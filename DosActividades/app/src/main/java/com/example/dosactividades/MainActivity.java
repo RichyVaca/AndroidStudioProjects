d988ea80514632c64de96f27dc2ac791ecaa478f
Ricardo Hiram Morales Vaca <32494582+RichyVaca@users.noreply.github.com>
2021-03-26 18:57:06 -0600

    activity2

--- DosActividades/app/src/main/java/com/example/dosactividades/MainActivity.java
+++ DosActividades/app/src/main/java/com/example/dosactividades/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.dosactividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button btnIr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIr = (Button)findViewById(R.id.btnA1);
+    }
+
+    /**
+     * Metodo para saltar entre actividades (pantallas)
+     */
+    public void saltarToA2(View vista){
+        //Creamos un objeto intent
+        Intent i = new Intent(this, MainActivity2.class);
+        //Ejecutar el intent
+        startActivity(i);
     }
 }
